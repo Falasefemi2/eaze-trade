ed99577102c4057ed8e3df3461147e63f8e6fc1e
Falasefemi2 <femifalase228@gmail.com>
2024-04-23 21:00:34 +0100

    boosted ads

---
+++ app/components/BoostedAdvert.tsx
@@ -0,0 +1,12 @@
+import { ImageSlider } from "@/components/component/image-slider";
+
+const BoostedAdvert = () => {
+    return (
+        <div>
+            <h1>Boosted Advert</h1>
+            <ImageSlider />
+        </div>
+    );
+}
+
+export default BoostedAdvert;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import BoostedAdvert from "./components/BoostedAdvert";
 import HeroBanner from "./components/Herobanner";
 
 export default function Home() {
   return (
     <main>
       <HeroBanner />
+      <BoostedAdvert />
     </main>
   );
 }
